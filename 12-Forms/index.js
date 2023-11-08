4628277b45e9b952335f295aa3436d154af96869
Andy Smith <andy.smith@qa.com>
2023-11-08 15:57:32 +0000

    Wednesday -12 Forms

---
+++ 12-Forms/index.js
@@ -0,0 +1,23 @@
+const nameInput = document.querySelector('[name="name"]')
+
+const validateNameLength = event =>{
+    if (event.target.value.length < 2){
+        console.log("name not long enough")
+        nameInput.focus()
+    }
+}
+
+nameInput.addEventListener('change', validateNameLength)
+
+const formSubmit = event =>{
+    event.preventDefault()
+    alert("The Form Was Submitted")
+    for (let i = 0; i < 3; i++){
+        console.log(event.target[i].value)
+    }
+}
+
+document.querySelector('form').addEventListener('submit', formSubmit)
+
+// document.querySelector('[name="email]').addEventListener('blur', check)
+const check = obj => obj.value.length < 1 ? obj.classList.add('error') : obj.classList.remove('error')
